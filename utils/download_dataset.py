8156455d36d2de14de81cec584c9c3774a08782c
GiorgioBono02 <s298940@studenti.polito.it>
2025-03-29 12:09:42 +0100

    Download_dataset finished

--- utils/download_dataset.py
+++ utils/download_dataset.py
@@ -2,6 +2,9 @@ import os
 import urllib.request
 import zipfile
 import shutil
+import torch
+from torchvision.datasets import ImageFolder
+import torchvision.transforms as T
 
 # URL del dataset
 dataset_url = "http://cs231n.stanford.edu/tiny-imagenet-200.zip"
@@ -43,3 +46,23 @@ with open(annotations_file, "r") as f:
 
 # Rimuoviamo la cartella images vuota
 shutil.rmtree(os.path.join(val_dir, "images"))
+
+transform = T.Compose([
+    T.Resize((224, 224)),  # Resize to fit the input dimensions of the network
+    T.ToTensor(),
+    T.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+    # mean and standard deviation for this dataset
+])
+
+# root folder / class name / name of the file of the parameters
+# root/{classX}/x001.jpg
+#root is the folder that contains all the data for the model
+tiny_imagenet_dataset_train = ImageFolder(root='tiny-imagenet/tiny-imagenet-200/train', transform=transform)
+tiny_imagenet_dataset_val = ImageFolder(root='tiny-imagenet/tiny-imagenet-200/val', transform=transform)
+
+print(f"Length of train dataset: {len(tiny_imagenet_dataset_train)}")
+print(f"Length of val dataset: {len(tiny_imagenet_dataset_val)}")
+
+# During training we are introducing some noise
+train_loader = torch.utils.data.DataLoader(tiny_imagenet_dataset_train, batch_size=32, shuffle=True, num_workers=8)
+val_loader = torch.utils.data.DataLoader(tiny_imagenet_dataset_val, batch_size=32, shuffle=False)
